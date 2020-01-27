d94ca9e660ef1c305fda15fc6847e4e36498279a
Marcel <marcelix161@gmail.com>
2020-01-27 23:43:25 +0100

    Added db Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """The reference is to the built-in Django User model."""
+    title = models.CharField(blank=True, max_length=256)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
