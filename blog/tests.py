2752bbc92fe7fb576a67a58a8fd9ccfe938fc343
Marcel <marcelix161@gmail.com>
2020-01-28 23:28:00 +0100

    Added setUp method to tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        self.post = Post.objects.create(
+            title='testtile',
+            text='test-text',
+            author=self.user)
+
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='testpassword')
