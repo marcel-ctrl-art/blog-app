1bae09334fb8c4fbabbbecd0e1abd3dce188b201
Marcel <marcelix161@gmail.com>
2020-01-28 02:38:09 +0100

    Added ListView of all Posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.views import View
+from .models import Post
 
-# Create your views here.
+
+class PostListView(View):
+    def get(self, request):
+        template_name = "posts_list.html"
+        posts = Post.objects.all().order_by("created_at")
+        paginator = Paginator(posts, 3)
+        page = request.GET.get("page")
+        posts = paginator.get_page(page)
+        ctx = {
+            "posts": posts
+        }
+        return render(request, template_name, ctx)
