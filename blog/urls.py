9054bcf7e3139301b5e887b95b9d1afca18480c3
Marcel <marcelix161@gmail.com>
2020-01-28 03:07:24 +0100

    Added url path for PostDetailView

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name="posts_list"),
+    path('post/<int:id>/', views.PostDetailView.as_view(), name="post_detail"),
 ]
