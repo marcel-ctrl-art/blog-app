8cf9b45cb5a72ff600972deceae9e518461722d7
Marcel <marcelix161@gmail.com>
2020-01-31 21:05:41 +0100

    Added Sign up View

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect, render
+from django.views import View
 
-# Create your views here.
+
+class SignUpView(View):
+    def get(self, request):
+        template_name = 'signup.html'
+        form = UserCreationForm
+        return render(request, template_name, {"form": form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+        return redirect('/accounts/login/')
