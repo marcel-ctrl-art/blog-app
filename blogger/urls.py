36d6675da86580e394054eaa66ab8d8d554e5d91
Marcel <marcelix161@gmail.com>
2020-01-31 17:14:43 +0100

    Included urls for accounts app

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+
+#The following order or urls matters here.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')),
 ]
