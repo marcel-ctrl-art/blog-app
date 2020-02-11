87344c57201ac3686b7c98c52ea8bb2f6a77baef
Marcel <marceli.lagoda@gmail.com>
2020-02-11 02:56:57 +0100

    Modify no. of post per page (paginator)

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ class PostListView(View):
     def get(self, request):
         template_name = "posts_list.html"
         posts = Post.objects.all().order_by("created_at")
-        paginator = Paginator(posts, 3)
+        paginator = Paginator(posts, 5)
         page = request.GET.get("page")
         posts = paginator.get_page(page)
         ctx = {
